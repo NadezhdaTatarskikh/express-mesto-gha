46281f53e7d42e1358d83e486181fb00ded629a6
N.Tatarskikh <nadya-garifyanova@yandex.ru>
2023-06-05 20:53:55 +0300

    ПР14 7 версия

--- routes/cards.js
+++ routes/cards.js
@@ -11,10 +11,10 @@ const {
   cardValidate, cardIdValidate,
 } = require('../middlewares/validation');
 
-router.get('/card', getCards); // возвращает все карточки
-router.post('/card/', cardValidate, createCard); // создаёт карточку
-router.delete('/card/:cardId', cardIdValidate, deleteCard); // удаляет карточку по идентификатору
-router.put('/card/:cardId/likes', cardIdValidate, likeCard); // поставить лайк карточке
-router.delete('/card/:cardId/likes', cardIdValidate, dislikeCard); // убрать лайк с карточки
+router.get('/', getCards); // возвращает все карточки
+router.post('/', cardValidate, createCard); // создаёт карточку
+router.delete('/:cardId', cardIdValidate, deleteCard); // удаляет карточку по идентификатору
+router.put('/:cardId/likes', cardIdValidate, likeCard); // поставить лайк карточке
+router.delete('/:cardId/likes', cardIdValidate, dislikeCard); // убрать лайк с карточки
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -13,10 +13,10 @@ const {
   userAvatarValidate,
 } = require('../middlewares/validation');
 
-router.get('/users', getUsers); // возвращает всех пользователей
-router.get('/users/:userId', userIdValidate, getUserById); // возвращает пользователя по _id
-router.get('/users/me', getCurrentUser); // возвращает информацию о текущем пользователе
-router.patch('/users/me/avatar', userAvatarValidate, updateAvatar); // обновляет аватар
-router.patch('/users/me', userInfoValidate, updateProfile); // обновляет профиль
+router.get('/', getUsers); // возвращает всех пользователей
+router.get('/:userId', userIdValidate, getUserById); // возвращает пользователя по _id
+router.get('/me', getCurrentUser); // возвращает информацию о текущем пользователе
+router.patch('/me/avatar', userAvatarValidate, updateAvatar); // обновляет аватар
+router.patch('/me', userInfoValidate, updateProfile); // обновляет профиль
 
 module.exports = router;
