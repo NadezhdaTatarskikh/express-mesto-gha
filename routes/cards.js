7189e5b70cd852d36d65f6fc1a0c9176c2014480
N.Tatarskikh <nadya-garifyanova@yandex.ru>
2023-06-05 19:53:28 +0300

    ПР14 6/2 версия

--- routes/cards.js
+++ routes/cards.js
@@ -11,10 +11,10 @@ const {
   cardValidate, cardIdValidate,
 } = require('../middlewares/validation');
 
-router.get('/', getCards); // возвращает все карточки
-router.post('/', cardValidate, createCard); // создаёт карточку
-router.delete('/:cardId', cardIdValidate, deleteCard); // удаляет карточку по идентификатору
-router.put('/:cardId/likes', cardIdValidate, likeCard); // поставить лайк карточке
-router.delete('/:cardId/likes', cardIdValidate, dislikeCard); // убрать лайк с карточки
+router.get('/cards', getCards); // возвращает все карточки
+router.post('/cards/', cardValidate, createCard); // создаёт карточку
+router.delete('/cards/:cardId', cardIdValidate, deleteCard); // удаляет карточку по идентификатору
+router.put('/cards/:cardId/likes', cardIdValidate, likeCard); // поставить лайк карточке
+router.delete('/cards/:cardId/likes', cardIdValidate, dislikeCard); // убрать лайк с карточки
 
 module.exports = router;
